3885fa694b6ba3b086c2c541d28f88af88c96b2b
Giel van Schijndel <giel@mortis.eu>
2024-12-01 16:54:24 +0100

    refactor: extract when_all to separate file

--- src/coro.cpp
+++ src/coro.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <olifilo/coro/future.hpp>
+#include <olifilo/coro/when_all.hpp>
 #include <olifilo/expected.hpp>
 #include <olifilo/io/connect.hpp>
 #include <olifilo/io/errors.hpp>
@@ -115,57 +116,6 @@ class file_descriptor
   private:
     io::file_descriptor_handle _fd;
 };
-
-template <typename... Ts>
-future<std::tuple<expected<Ts>...>> when_all(future<Ts>... futures) noexcept
-{
-  ////std::string_view func_name(__PRETTY_FUNCTION__);
-  ////func_name = func_name.substr(func_name.find("when_all"));
-
-  auto& my_promise = co_await my_current_promise();
-  assert(my_promise.events.empty());
-  assert(my_promise.root_caller == &my_promise);
-
-  // Force promise.await_transform(await-expr) to be executed for all futures *before* suspending execution of *this* coroutine when invoking co_await.
-  // Unfortunately whether the co_await pack expansion executes in this order or once per future just before suspending for each future is implementation-defined. So we need this hack...
-  (my_promise.await_transform(std::move(futures)), ...);
-
-  // Now allow this future's .get() to handle the actual I/O multiplexing
-  co_return std::tuple<expected<Ts>...>((co_await futures)...);
-}
-
-template <std::forward_iterator I, std::sentinel_for<I> S>
-requires(is_future_v<typename std::iterator_traits<I>::value_type>)
-future<std::vector<typename std::iterator_traits<I>::value_type::value_type>> when_all(I first, S last) noexcept
-{
-  ////std::string_view func_name(__PRETTY_FUNCTION__);
-  ////func_name = func_name.substr(func_name.find("when_all"));
-
-  std::vector<typename std::iterator_traits<I>::value_type::value_type> rv;
-
-  auto& my_promise = co_await my_current_promise();
-
-  // Force promise.await_transform(await-expr) to be executed for all futures *before* suspending execution of *this* coroutine when invoking co_await.
-  std::size_t count = 0;
-  for (auto i = first; i != last; ++i, ++count)
-    *i = my_promise.await_transform(std::move(*i));
-
-  rv.reserve(count);
-  // Now allow this future's .get() to handle the actual I/O multiplexing while collecting the results
-  for (; first != last; ++first)
-    rv.emplace_back(co_await *first);
-
-  co_return rv;
-}
-
-template <std::ranges::forward_range R>
-requires(std::is_rvalue_reference_v<R&&>)
-auto when_all(R&& futures) noexcept
-{
-  using std::ranges::begin;
-  using std::ranges::end;
-  return when_all(begin(futures), end(futures));
-}
 
 future<void> sleep_until(io::poll::timeout_clock::time_point time) noexcept
 {
