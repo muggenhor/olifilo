f8e94a3981daf18ab12c0122171442b03b2d4e25
Giel van Schijndel <giel@mortis.eu>
2024-12-16 00:49:46 +0100

    refactor(wait): simplify it by just scanning the list of promises fully each time
    Instead of jumping through a bunch of hoops to optimize scenarios with
    large numbers of futures being waited on in parallel... Which happens
    *not* to be our case.
    
    This significantly improves readability, probably at some cost for huge
    numbers of awaited promises. But also probably at a performance
    improvement for tiny numbers.

--- src/coro/wait.cpp
+++ src/coro/wait.cpp
@@ -3,6 +3,7 @@
 #include "olifilo/coro/wait.hpp"
 
 #include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <ranges>
 
@@ -20,31 +21,6 @@ future<std::size_t>
   // Special case because the code below assumes that .begin() + 1 and .end() - 1 are valid iterators.
   if (promises.empty())
     co_return {std::in_place, 0};
-
-  /******************
-   * Input scanning *
-   ******************/
-  constexpr auto ready_input = [] (auto fut) noexcept { return fut == nullptr; };
-  constexpr auto not_ready_input = [ready_input] (auto fut) noexcept { return !ready_input(fut); };
-
-  expected<std::size_t> first_ready_future = std::ranges::find_if(promises, ready_input) - promises.begin();
-  // Special case to avoid even looking at other promises if we need a single and found one ready
-  if (wait_until == until::first_completed
-   && *first_ready_future != promises.size())
-    co_return first_ready_future;
-
-  const auto first_not_ready_future_index = *first_ready_future != 0
-    ? 0
-    : std::ranges::find_if(promises.begin() + 1, promises.end(), not_ready_input) - promises.begin()
-    ;
-  assert(first_not_ready_future_index == std::ranges::find_if(promises, not_ready_input) - promises.begin());
-  const auto last_not_ready_future_index = std::ranges::find_if(
-      std::reverse_iterator(promises.end())
-    , std::reverse_iterator(promises.begin() + first_not_ready_future_index)
-    , not_ready_input
-    ).base() - promises.begin();
-  assert(last_not_ready_future_index == std::ranges::find_if(promises | std::ranges::views::reverse, not_ready_input).base() - promises.begin());
-  assert(first_not_ready_future_index <= last_not_ready_future_index);
 
   auto& my_promise = co_await detail::current_promise();
 
@@ -73,15 +49,13 @@ future<std::size_t>
         if (!future)
           continue;
 
-        // Restore any remaining promises to the top of their callgraphs
-        future->caller = nullptr;
+        // Restore promises to being the top of their respective await call graphs with nothing waiting on them (anymore)
         future->waits_on_me = nullptr;
       }
     }
   } scope_exit(my_promise.callees, promises);
 
-  constexpr auto ready = [] (auto future) noexcept { return future->caller == nullptr; };
-  constexpr auto not_ready = [ready] (auto future) noexcept { return !ready(future); };
+  constexpr auto ready = [] (auto future) noexcept { return future->waits_on_me == nullptr; };
 
   assert(ready(&nop_always_ready_promise));
 
@@ -89,17 +63,17 @@ future<std::size_t>
   const auto me = std::coroutine_handle<std::remove_cvref_t<decltype(my_promise)>>::from_promise(my_promise);
   for (auto*& future : my_promise.callees)
   {
-    if (ready_input(future))
+    if (const bool ready_input = future == nullptr;
+        ready_input)
     {
-      // const_cast is safe because this function won't modify anything with caller == nullptr (the default).
-      // And executors are only allowed to *remove* entries from the 'events' list, which for this is empty.
+      // const_cast is safe because this function takes care not to modify it through this list
+      // And executors are only allowed to *remove* entries from the 'events' list, which for this NOP promise is empty.
       future = const_cast<detail::promise_wait_callgraph*>(&nop_always_ready_promise);
       continue;
     }
 
     assert(future->waits_on_me == nullptr && "internal logic error: not allowed to await promises already being awaited");
     assert(future->caller == nullptr && "stealing a future someone else is waiting on");
-    future->caller = &my_promise;
     future->waits_on_me = me;
   }
 
@@ -114,29 +88,26 @@ future<std::size_t>
       co_return {unexpect, r.error()};
   }
 
-  // Restore promises to being the top of their respective await call graphs with nothing waiting on them (anymore)
-  const auto mark_ready = [=, caller = &my_promise] (auto future) noexcept {
-    assert(future->caller == caller);
-    assert(future->waits_on_me == nullptr || future->waits_on_me == me);
-    future->caller = nullptr;
-    future->waits_on_me = nullptr;
-  };
+  /***************************************************************************************************
+   * Scan the list of promises to see if enough are ready, suspend ourselves, detect timeout, repeat *
+   ***************************************************************************************************/
 
-  /***********************************************************************************************
-   * Suspend ourselves, detect timeout, and scan the list of promises to see if enough are ready *
-   ***********************************************************************************************/
-
-  auto first_not_ready_future = my_promise.callees.begin() + first_not_ready_future_index;
-  auto last_not_ready_future = my_promise.callees.begin() + last_not_ready_future_index;
-  while (first_not_ready_future != last_not_ready_future)
+  while (true)
   {
-    assert(my_promise.callees.begin() <= first_not_ready_future);
-    assert(first_not_ready_future < last_not_ready_future);
-    assert(last_not_ready_future <= my_promise.callees.end());
-
     assert(std::ranges::empty(my_promise.events) && "TODO: test timeouts!");
-    assert(first_ready_future);
-    assert(wait_until != until::first_completed || *first_ready_future == my_promise.callees.size());
+
+    bool all_ready = true;
+    for (const auto [index, future] : my_promise.callees | std::views::enumerate)
+    {
+      assert(ready(future) || future->waits_on_me == me);
+
+      if (!ready(future))
+        all_ready = false;
+      else if (wait_until == until::first_completed)
+        co_return {std::in_place, index};
+    }
+    if (all_ready)
+      co_return {std::in_place, 0};
 
     // Now allow this future's .get() to handle the actual I/O multiplexing
     co_await std::suspend_always();
@@ -145,52 +116,9 @@ future<std::size_t>
      && timeout_event->waiter == nullptr)
     {
       // Timeout occurred
-      first_ready_future = {unexpect, timeout_event->wait_result.error()};
       assert(std::ranges::find(my_promise.events, &*timeout_event) == my_promise.events.end() && "executor should have removed timeout event from our wait list");
-      goto stop_waiting;
-    }
-
-    for (auto future = first_not_ready_future; future != last_not_ready_future; ++future)
-    {
-      if (not_ready(*future))
-      {
-        assert((*future)->caller == &my_promise);
-        assert((*future)->waits_on_me == nullptr || (*future)->waits_on_me == me);
-
-        if (const bool became_ready = (*future)->waits_on_me == nullptr;
-            became_ready)
-        {
-          if (const auto index = static_cast<std::size_t>(future - my_promise.callees.begin());
-              *first_ready_future > index)
-          {
-            first_ready_future.emplace(index);
-            if (wait_until == until::first_completed)
-              goto stop_waiting;
-          }
-
-          mark_ready(*future);
-        }
-      }
-
-      // Update list [begin,end] markers to ensure they'll be equal when we only have ready promises left
-      if (ready(*future))
-      {
-        if (future == first_not_ready_future)
-        {
-          ++first_not_ready_future;
-        }
-        else if (future + 1 == last_not_ready_future)
-        {
-          --last_not_ready_future;
-          break;
-        }
-      }
+      co_return {unexpect, timeout_event->wait_result.error()};
     }
   }
-stop_waiting:
-  assert(!first_ready_future || *first_ready_future != my_promise.callees.size());
-  assert(!first_ready_future || wait_until != until::all_completed || *first_ready_future == 0);
-
-  co_return first_ready_future;
 }
 }  // namespace olifilo
