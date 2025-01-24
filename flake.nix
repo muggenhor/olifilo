39694433c19d2b28a8ef8d9b92e3fe58ec8af693
Giel van Schijndel <giel@mortis.eu>
2025-01-24 21:35:02 +0100

    build: and run ESP-IDF example with QEMU

--- flake.nix
+++ flake.nix
@@ -8,8 +8,11 @@
     inputs.nixpkgs.follows = "nixpkgs";
     inputs.flake-utils.follows = "flake-utils";
   };
+  inputs.esp-qemu = {
+    url = "github:SFrijters/nix-qemu-espressif";
+  };
 
-  outputs = inputs@{ self, esp-idf-dev, flake-utils, nixpkgs, ... }:
+  outputs = inputs@{ self, esp-idf-dev, esp-qemu, flake-utils, nixpkgs, ... }:
   flake-utils.lib.eachDefaultSystem (system:
   let
     versionOf = flake:
@@ -66,10 +69,12 @@
       dontPatchELF = true;
       dontStrip = true;
     };
-  in {
-    packages = {
+  in rec {
+    packages = rec {
       inherit olifilo;
       default = olifilo;
+      inherit (esp-qemu.packages.${system}) qemu-espressif qemu-esp32 qemu-esp32c3;
+      qemu-esp32s3 = qemu-esp32;
     } // builtins.listToAttrs (
       map (idf-target: {
         name = "${idf-target}-olifilo";
@@ -83,5 +88,24 @@
         };
       }) [ "esp32" "esp32c2" "esp32c3" "esp32s2" "esp32s3" "esp32c6" "esp32h2" ]
     );
+
+    checks = {
+      esp32s3-qemu = pkgs.runCommand "esp32s3-olifilo-qemu.log" {} ''
+        set -x
+        ${pkgs.lib.escapeShellArg (pkgs.lib.getExe pkgs.espflash)} save-image --chip esp32s3 --merge ${pkgs.lib.escapeShellArg packages.esp32s3-olifilo}/bin/olifilo.elf --bootloader ${pkgs.lib.escapeShellArg packages.esp32s3-olifilo}/libexec/olifilo/bootloader.bin --flash-size 4mb esp32s3-olifilo.img
+        ${pkgs.lib.escapeShellArg (pkgs.lib.getExe pkgs.esptool)} image_info --version 2 esp32s3-olifilo.img
+        ${pkgs.lib.escapeShellArg (pkgs.lib.getExe packages.qemu-esp32s3)} -nographic -monitor unix:monitor.sock,server,nowait -machine esp32s3 -drive file=esp32s3-olifilo.img,if=mtd,format=raw -m 2M -serial "file:$out" &
+        sleep 3
+        echo 'quit' | ${pkgs.lib.escapeShellArg (pkgs.lib.getExe pkgs.netcat)} -N -U monitor.sock
+        wait
+        cat "$out"
+        if grep -q '^Backtrace' "$out"; then
+          exit 1
+        fi
+        grep 'ESP-ROM:esp32s3-20210327' "$out"
+        grep 'Calling app_main()' "$out"
+        set +x
+      '';
+    };
   });
 }
