137cf893063f439867e3ac0d1d42b9a8b3c82207
Giel van Schijndel <giel@mortis.eu>
2025-01-31 12:26:43 +0100

    refactor(dynarray): store element count cookie at beginning
    Instead of at the end, where we depended on T's alignment being a
    multiple of size_t's alignment (which doesn't hold for smaller Ts like
    char).
    
    Yes, we could have just added padding. But at the end that would have
    been variable-sized padding. At the beginning it's a constant amount
    that simplifies address computation.

--- include/olifilo/dynarray.hpp
+++ include/olifilo/dynarray.hpp
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 #include <memory>
 #include <new>
@@ -19,87 +20,106 @@ class dynarray
   public:
     using value_type = T;
 
+  private:
+    static constexpr std::size_t cookie_size = std::max(sizeof(std::size_t), alignof(value_type));
+    struct alloc_align_t
+    {
+      alignas(std::min(sizeof(value_type), cookie_size)) std::byte buf[std::min(sizeof(value_type), cookie_size)];
+    };
+
+  public:
     static constexpr expected<dynarray> create(std::size_t count) noexcept
     {
       if (count == 0)
         return dynarray();
 
       // avoid overflow
-      if (count > (std::size_t(-1) - sizeof(std::size_t)) / sizeof(value_type))
+      if (count > (std::size_t(-1) - cookie_size) / sizeof(value_type))
         return {unexpect, make_error_code(std::errc::invalid_argument)};
 
-      const std::size_t size = count * sizeof(value_type) + sizeof(std::size_t);
-      auto buf = new (std::nothrow) std::byte[size];
+      static_assert(sizeof(value_type) % sizeof(alloc_align_t) == 0);
+      static_assert(cookie_size % sizeof(alloc_align_t) == 0);
+      const std::size_t size = count * (sizeof(value_type) / sizeof(alloc_align_t)) + cookie_size / sizeof(alloc_align_t);
+      auto buf = new (std::nothrow) alloc_align_t[size];
       if (!buf)
         return {unexpect, make_error_code(std::errc::not_enough_memory)};
 
       // store count *after* data to ensure it doesn't screw with alignment of data
-      const auto countp = static_cast<std::size_t*>(static_cast<void*>(buf + count * sizeof(value_type)));
-      static_assert(alignof(value_type) % alignof(*countp) == 0);
+      const auto countp = static_cast<std::size_t*>(static_cast<void*>(buf->buf + cookie_size - sizeof(std::size_t)));
       std::construct_at(countp, count);
 
-      std::ranges::uninitialized_default_construct(std::span(static_cast<value_type*>(static_cast<void*>(buf)), count));
-      return dynarray(countp);
+      const auto datap = static_cast<value_type*>(static_cast<void*>(buf->buf + cookie_size));
+      std::uninitialized_default_construct_n(datap, count);
+      return dynarray(datap);
     }
 
-    constexpr dynarray() noexcept
-      : _count(nullptr)
-    {
-    }
+    dynarray() = default;
 
-    explicit constexpr dynarray(void* count) noexcept
-      : _count(static_cast<std::size_t*>(count))
+    explicit constexpr dynarray(void* data) noexcept
+      : _data(static_cast<value_type*>(data))
     {
     }
 
     constexpr dynarray(dynarray&& rhs) noexcept
-      : _count(std::exchange(rhs._count, nullptr))
+      : _data(std::exchange(rhs._data, nullptr))
     {
     }
 
     constexpr dynarray& operator=(dynarray&& rhs) noexcept
     {
       dynarray _(std::move(*this));
-      _count = std::exchange(rhs._count, nullptr);
+      _data = std::exchange(rhs._data, nullptr);
       return *this;
     }
 
     constexpr ~dynarray()
     {
-      if (_count)
+      if (_data)
       {
-        std::destroy(begin(), end());
-        auto buf = static_cast<std::byte*>(static_cast<void*>(_count)) - *_count * sizeof(value_type);
-        std::destroy_at(_count);
+        const auto countp = static_cast<const std::size_t*>(static_cast<const void*>(_data)) - 1;
+        std::destroy_n(_data, *countp);
+        std::destroy_at(countp);
+
+        const auto buf = static_cast<alloc_align_t*>(static_cast<void*>(static_cast<std::byte*>(static_cast<void*>(_data)) - cookie_size));
         delete [] buf;
       }
     }
 
     constexpr void* release() noexcept
     {
-      return std::exchange(_count, nullptr);
+      return std::exchange(_data, nullptr);
     }
 
-    template <typename Self>
-    constexpr auto end(this Self&& self) noexcept -> cv_like_t<Self, value_type*>
+    constexpr bool empty() const noexcept
     {
-      return static_cast<value_type*>(static_cast<void*>(self._count));
+      return !_data;
+    }
+
+    constexpr std::size_t size() const noexcept
+    {
+      if (!_data)
+        return 0;
+      return *(static_cast<const std::size_t*>(static_cast<const void*>(_data)) - 1);
     }
 
     template <typename Self>
     constexpr auto begin(this Self&& self) noexcept -> cv_like_t<Self, value_type*>
     {
-      if (!self._count)
-        return nullptr;
-      return std::forward<Self>(self).end() - *self._count;
+      return self._data;
+    }
+
+    template <typename Self>
+    constexpr auto end(this Self&& self) noexcept -> cv_like_t<Self, value_type*>
+    {
+      return self._data + self.size();
     }
 
   private:
-    explicit constexpr dynarray(std::size_t* count) noexcept
-      : _count(count)
+    explicit constexpr dynarray(value_type* count) noexcept
+      : _data(count)
     {
     }
 
-    std::size_t* _count;
+    value_type* _data = nullptr;
 };
 }  // namespace olifilo
