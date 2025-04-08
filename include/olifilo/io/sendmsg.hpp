a8e9b8334104e13c3982b96b9c6495ba739336f7
Giel van Schijndel <giel@mortis.eu>
2025-04-09 00:08:37 +0200

    feat(io): add 'io::sendmsg' wrapper similar to 'io::write' with iovec support
    The current implementation depends on 'struct iovec' and
    'std::span<T, dynamic>' having the same layout. This avoids copying, but
    is probably undefined behavior.
    
    It also doesn't support any of the other features of sendmsg, such as:
    1. specifying target address to send to (for connectionless sockets)
    2. sending ancillary data

---
+++ include/olifilo/io/sendmsg.hpp
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <cerrno>
+#include <cstddef>
+#include <limits>
+#include <span>
+#include <system_error>
+
+#include <sys/socket.h>
+
+#include "../expected.hpp"
+#include "types.hpp"
+
+namespace olifilo::io
+{
+inline expected<std::size_t> sendmsg(file_descriptor_handle fd, std::span<const std::span<const std::byte>> bufs, int flags) noexcept
+{
+  if (bufs.size() > static_cast<std::size_t>(std::numeric_limits<::msg_iovlen_t>::max()))
+    return {olifilo::unexpect, make_error_code(std::errc::message_size)};
+
+  // FIXME: this relies on 'struct iovec' and 'std::span<T>' having the same layout. While currently true on
+  //        libstdc++ (GCC 14) we might wish to static_assert that somehow.
+  const ::msghdr msg = {
+    .msg_iov = const_cast<::iovec*>(reinterpret_cast<const ::iovec*>(bufs.data())),
+    .msg_iovlen = static_cast<::msg_iovlen_t>(bufs.size()),
+  };
+
+  if (auto rv = ::sendmsg(fd, &msg, flags);
+      rv == -1)
+    return std::error_code(errno, std::system_category());
+  else
+    return static_cast<std::size_t>(rv);
+}
+}  // namespace olifilo::io
