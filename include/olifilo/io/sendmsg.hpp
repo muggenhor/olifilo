4c05c2dacefb257032d4ba0b662a854a4a22beaa
Giel van Schijndel <giel@mortis.eu>
2025-04-09 21:17:46 +0200

    fix(sendmsg): don't rely on existence of non-standard type 'msg_iovlen_t'

--- include/olifilo/io/sendmsg.hpp
+++ include/olifilo/io/sendmsg.hpp
@@ -17,14 +17,17 @@ namespace olifilo::io
 {
 inline expected<std::size_t> sendmsg(file_descriptor_handle fd, std::span<const std::span<const std::byte>> bufs, int flags) noexcept
 {
-  if (bufs.size() > static_cast<std::size_t>(std::numeric_limits<::msg_iovlen_t>::max()))
+  // While this is size_t on POSIX, it's a custom type on lwIP
+  using iovlen_t = decltype(::msghdr::msg_iovlen);
+
+  if (bufs.size() > static_cast<std::size_t>(std::numeric_limits<iovlen_t>::max()))
     return {olifilo::unexpect, make_error_code(std::errc::message_size)};
 
   // FIXME: this relies on 'struct iovec' and 'std::span<T>' having the same layout. While currently true on
   //        libstdc++ (GCC 14) we might wish to static_assert that somehow.
   const ::msghdr msg = {
     .msg_iov = const_cast<::iovec*>(reinterpret_cast<const ::iovec*>(bufs.data())),
-    .msg_iovlen = static_cast<::msg_iovlen_t>(bufs.size()),
+    .msg_iovlen = static_cast<iovlen_t>(bufs.size()),
   };
 
   if (auto rv = ::sendmsg(fd, &msg, flags);
